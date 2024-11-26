662632988f95e2f18a2db6dc5649211855992c9c
tencho <tencho@gmail.com>
2024-11-26 11:35:13 -0300

    Fix

--- chunk-H7DORFQP.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-EUDKD2JW.js";var t=[{path:"",loadComponent:()=>import("./chunk-BUOQRMBC.js").then(o=>o.DashboardComponent),data:{title:$localize`Dashboard`}}];export{t as routes};
--- chunk-SMZTGISE.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-EUDKD2JW.js";var o=[{path:"",data:{title:"Extensiones"},children:[{path:"",redirectTo:"listado",pathMatch:"full"},{path:"listado",loadComponent:()=>import("./chunk-JHEA23XG.js").then(t=>t.ListadoExtensionesComponent),data:{title:"Listado"}}]}];export{o as routes};
