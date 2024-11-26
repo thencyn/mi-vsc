cf3e51d619597ee32fbf849eeea0d7a582bfa071
tencho <tencho@gmail.com>
2024-11-25 23:41:17 -0300

    Initial commit

---
+++ chunk-LAPZSMLI.js
@@ -0,0 +1,20 @@
+import{Bb as e,Cb as i,Db as o,Tb as t,Vb as r,_a as d,_b as c,ga as s}from"./chunk-LKICGNUI.js";import"./chunk-EUDKD2JW.js";var u=(()=>{let n=class n{constructor(){this.div1='<div class="nombre-clase"></div>',this.div2="<div><span>hola mundo</span></div>",this.div3=`<div>
+  <table>
+    <thead>
+      <tr>
+        <th></th>
+        <th></th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+    </tbody>
+  </table>
+</div>`}};n.\u0275fac=function(a){return new(a||n)},n.\u0275cmp=s({type:n,selectors:[["app-editor-emmet"]],standalone:!0,features:[c],decls:29,vars:3,consts:[["href","https://docs.emmet.io/cheat-sheet/","target","_blank"],[1,"mt-3"]],template:function(a,l){a&1&&(e(0,"h1"),t(1,"Emmet"),i(),e(2,"p"),t(3,` Emmet es una poderosa herramienta integrada en VSC que facilita y acelera la escritura de c\xF3digo HTML/CSS mediante atajos o abreviaciones. Con Emmet, puedes generar porciones completas de c\xF3digo con tan solo unas pocas instrucciones.
+`),i(),e(4,"p"),t(5,` Visual Studio Code incluye Emmet por defecto, sin necesidad de instalar extensiones adicionales. Les recomiendo ver la documentaci\xF3n oficial de Emmet para conocer todas las abreviaciones y atajos disponibles.
+`),i(),e(6,"a",0),t(7,"Cheat Sheet"),i(),e(8,"div",1),t(9," Sin duda el que mas utilizaremos: "),e(10,"code"),t(11,".nombre-clase"),i(),o(12,"br"),e(13,"code"),t(14),i()(),e(15,"div",1),t(16," Ejemplo div: "),e(17,"code"),t(18,"div>span{hola mundo}"),i(),o(19,"br"),e(20,"code"),t(21),i()(),e(22,"div",1),t(23," Ejemplo mas complejo: "),e(24,"code"),t(25,"div>table>(thead>tr>th*3)+tbody>tr>td*3"),i(),o(26,"br"),e(27,"code"),t(28),i()()),a&2&&(d(14),r(" ",l.div1," "),d(7),r(" ",l.div2," "),d(7),r(" ",l.div3," "))}});let m=n;return m})();export{u as EditorEmmetComponent};
